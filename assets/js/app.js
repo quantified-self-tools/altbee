80c0b3a6bb3a375b4d5fa85df0759c01dd0601e2
Benjamin Fox <foxbenjaminfox@gmail.com>
2020-08-24 19:46:40 +0300

    Add dropdown with Beeminder goal page link to goal cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 import '../css/app.css'
 
 import 'phoenix_html'
+import 'alpinejs'
 
 import './forms'
 
@@ -9,7 +10,14 @@ import NProgress from 'nprogress'
 import { LiveSocket } from 'phoenix_live_view'
 
 const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute('content')
-const liveSocket = new LiveSocket('/live', Socket, { params: { _csrf_token: csrfToken } })
+const liveSocket = new LiveSocket('/live', Socket, {
+  dom: {
+    onBeforeElUpdated (from, to) {
+      if (from.__x) { window.Alpine.clone(from.__x, to) }
+    }
+  },
+  params: { _csrf_token: csrfToken }
+})
 
 window.addEventListener('phx:page-loading-start', info => NProgress.start())
 window.addEventListener('phx:page-loading-stop', info => NProgress.done())
--- lib/altbee_web/live/home_live/goal_dropdown_component.ex
+++ lib/altbee_web/live/home_live/goal_dropdown_component.ex
@@ -1,3 +1,11 @@
 defmodule AltbeeWeb.HomeLive.GoalDropdownComponent do
   use AltbeeWeb, :live_component
+
+  def menu_link_class do
+    "flex items-center px-4 py-2 text-sm leading-5 text-gray-700 hover:underline focus:bg-cool-gray-100 hover:text-gray-800 focus:outline-none focus:text-gray-800 group"
+  end
+
+  def beeminder_url(%{user: user, goal: goal}) do
+    "https:/www.beeminder.com/#{user.username}/#{goal["slug"]}"
+  end
 end
