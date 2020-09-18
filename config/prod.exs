ae1692e2e31343e5cb071dcaed7b4bf2aaefbcf9
Benjamin Fox <foxbenjaminfox@gmail.com>
2020-09-18 11:15:30 +0300

    Control the host config via an environment variable

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,14 @@
 use Mix.Config
 
+host =
+  System.get_env("ALTBEE_HOST") ||
+    raise """
+    environment variable ALTBEE_HOST is missing.
+    for example: altbee.aeonc.com
+    """
+
 config :altbee, AltbeeWeb.Endpoint,
-  url: [host: "altbee.aeonc.com", port: 443, scheme: "https"],
+  url: [host: host, port: 443, scheme: "https"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :logger, level: :info
