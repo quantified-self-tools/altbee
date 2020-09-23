251220253d7f46a5663be41e59f5a5025eda73a9
Benjamin Fox <foxbenjaminfox@gmail.com>
2020-09-23 23:26:50 +0300

    Tweak styling of goal dropdown

--- lib/altbee_web/live/home_live/goal_dropdown_component.ex
+++ lib/altbee_web/live/home_live/goal_dropdown_component.ex
@@ -2,7 +2,11 @@ defmodule AltbeeWeb.HomeLive.GoalDropdownComponent do
   use AltbeeWeb, :live_component
 
   def menu_link_class do
-    "flex items-center px-4 py-2 text-sm leading-5 text-gray-700 hover:underline focus:bg-cool-gray-100 hover:text-gray-800 focus:outline-none focus:text-gray-800 group"
+    "flex items-center px-4 py-2 w-full
+    text-sm leading-5 text-gray-700 group
+    hover:bg-indigo-500 hover:text-white
+    active:bg-indigo-500 active:text-white
+    focus:outline-none focus:bg-indigo-500 focus:text-white"
   end
 
   def beeminder_url(%{user: user, goal: goal}) do
