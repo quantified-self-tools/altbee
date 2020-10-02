94b2961f9de4cea3c78d6cb3e4074c4204da17e8
Benjamin Fox <foxbenjaminfox@gmail.com>
2020-10-02 12:21:28 +0300

    Add datapoint deletion confirmation modal

---
+++ lib/altbee_web/live/goal_live/confirm_datapoint_delete_component.ex
@@ -0,0 +1,3 @@
+defmodule AltbeeWeb.GoalLive.ConfirmDatapointDeleteComponent do
+  use AltbeeWeb, :live_component
+end
--- lib/altbee_web/live/goal_live/datapoints_component.ex
+++ lib/altbee_web/live/goal_live/datapoints_component.ex
@@ -1,3 +1,5 @@
 defmodule AltbeeWeb.GoalLive.DatapointsComponent do
   use AltbeeWeb, :live_component
+
+  alias AltbeeWeb.GoalLive.ConfirmDatapointDeleteComponent
 end
