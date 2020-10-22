b2a88e35163eabc131c0fcb0f26818e1877e2915
Benjamin Fox <foxbenjaminfox@gmail.com>
2020-10-22 22:50:17 +0300

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule Altbee.MixProject do
       {:floki, ">= 0.0.0", only: :test},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2.0"},
+      {:phoenix_live_dashboard, "~> 0.3.2"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:jason, "~> 1.0"},
