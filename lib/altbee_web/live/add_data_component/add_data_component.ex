85640b372b841c1a7f1a9d9d48573f8f8decf314
Benjamin Fox <foxbenjaminfox@gmail.com>
2021-02-17 20:42:00 +0200

    Factor out data entry component

---
+++ lib/altbee_web/live/add_data_component/add_data_component.ex
@@ -0,0 +1,105 @@
+defmodule AltbeeWeb.AddDataComponent do
+  use AltbeeWeb, :live_component
+
+  alias Altbee.Datapoints
+  import Altbee.Datapoints, only: [parse_datapoint: 1]
+
+  @datapoint_number_parse_error_msg "Your datapoint should be a number."
+  @datapoint_time_parse_error_msg "Your datapoint should be a number or a time."
+  @datapoint_empty_msg "Enter a value for your datapoint."
+  @datapoint_too_big_msg "Your datapoint must be less than 3,486,784,401"
+
+  def day_text_from_diff(diff, timezone, deadline) do
+    date = date_from_diff(diff, timezone, deadline)
+
+    case diff do
+      0 ->
+        "Today (#{date})"
+
+      1 ->
+        "Yesterday (#{date})"
+
+      _ ->
+        day = Timex.day_name(Date.day_of_week(date))
+        "#{day} (#{date})"
+    end
+  end
+
+  defp date_from_diff(diff, timezone, deadline) do
+    Timex.now()
+    |> Timex.Timezone.convert(timezone)
+    |> Timex.shift(days: -diff)
+    |> Timex.shift(seconds: -deadline)
+    |> Timex.to_date()
+  end
+
+  def daystamp_from_diff(diff, timezone, deadline) do
+    date_from_diff(diff, timezone, deadline)
+    |> Timex.format!("{YYYY}{0M}{0D}")
+  end
+
+  def new_data_placeholder(%{"last_datapoint" => %{"value" => last_datapoint_value}}) do
+    "e.g. #{display_value(last_datapoint_value)}"
+  end
+
+  def new_data_placeholder(_) do
+    "e.g. 1"
+  end
+
+  def mount(socket) do
+    socket =
+      socket
+      |> assign(:datapoint_parse_error, nil)
+
+    {:ok, socket}
+  end
+
+  def handle_event(
+        "new-datapoint",
+        %{"comment" => comment, "daystamp" => daystamp, "value" => value},
+        socket
+      ) do
+    socket =
+      case parse_datapoint(value) do
+        {:ok, value} ->
+          if value >= 3_486_784_401 do
+            assign(socket, :datapoint_parse_error, @datapoint_too_big_msg)
+          else
+            access_token = socket.assigns.user.access_token
+            slug = socket.assigns.goal["slug"]
+
+            send(self(), :new_datapoint)
+
+            Datapoints.submit_datapoint!(slug, access_token, daystamp, value, comment)
+
+            socket
+            |> assign(:datapoint_parse_error, nil)
+          end
+
+        {:error, :empty} ->
+          assign(socket, :datapoint_parse_error, @datapoint_empty_msg)
+
+        {:error, :number} ->
+          assign(socket, :datapoint_parse_error, @datapoint_number_parse_error_msg)
+
+        {:error, :time} ->
+          assign(socket, :datapoint_parse_error, @datapoint_time_parse_error_msg)
+      end
+
+    {:noreply, socket}
+  end
+
+  def handle_event("new-datapoint-change", %{"value" => value}, socket) do
+    socket =
+      if socket.assigns.datapoint_parse_error do
+        case parse_datapoint(value) do
+          {:ok, _} -> assign(socket, :datapoint_parse_error, nil)
+          {:error, _} -> socket
+        end
+      else
+        socket
+      end
+
+    {:noreply, socket}
+  end
+end
--- lib/altbee_web/live/goal_live/goal_live.ex
+++ lib/altbee_web/live/goal_live/goal_live.ex
@@ -4,14 +4,10 @@ defmodule AltbeeWeb.GoalLive do
   require Logger
 
   @goal_poll_interval 400
-  @datapoint_number_parse_error_msg "Your datapoint should be a number."
-  @datapoint_time_parse_error_msg "Your datapoint should be a number or a time."
-  @datapoint_empty_msg "Enter a value for your datapoint."
-  @datapoint_too_big_msg "Your datapoint must be less than 3,486,784,401"
 
   alias Altbee.{Datapoints, Goals}
-  import Altbee.Datapoints, only: [parse_datapoint: 1]
 
+  alias AltbeeWeb.AddDataComponent
   alias __MODULE__.DatapointsComponent
 
   def mount(%{"slug" => slug}, %{"user_id" => user_id}, socket) do
@@ -28,7 +24,6 @@ defmodule AltbeeWeb.GoalLive do
 
         socket
         |> assign(:waiting, false)
-        |> assign(:datapoint_parse_error, nil)
         |> assign(:slug, slug)
         |> assign(:page_title, "#{slug} · #{user.username}")
         |> assign(:goal, goal)
@@ -38,70 +33,12 @@ defmodule AltbeeWeb.GoalLive do
     {:ok, socket}
   end
 
-  def new_data_placeholder(%{"last_datapoint" => %{"value" => last_datapoint_value}}) do
-    "e.g. #{display_value(last_datapoint_value)}"
-  end
-
-  def new_data_placeholder(_) do
-    "e.g. 1"
-  end
-
-  def handle_event(
-        "new-datapoint",
-        %{"comment" => comment, "daystamp" => daystamp, "value" => value},
-        socket
-      ) do
-    socket =
-      case parse_datapoint(value) do
-        {:ok, value} ->
-          if value >= 3_486_784_401 do
-            assign(socket, :datapoint_parse_error, @datapoint_too_big_msg)
-          else
-            access_token = socket.assigns.user.access_token
-            slug = socket.assigns.slug
-
-            enqueue_goal_poll(socket)
-
-            Datapoints.submit_datapoint!(slug, access_token, daystamp, value, comment)
-
-            socket
-            |> assign(:datapoint_parse_error, nil)
-            |> assign(:waiting, true)
-          end
-
-        {:error, :empty} ->
-          assign(socket, :datapoint_parse_error, @datapoint_empty_msg)
-
-        {:error, :number} ->
-          assign(socket, :datapoint_parse_error, @datapoint_number_parse_error_msg)
-
-        {:error, :time} ->
-          assign(socket, :datapoint_parse_error, @datapoint_time_parse_error_msg)
-      end
-
-    {:noreply, socket}
-  end
-
-  def handle_event("new-datapoint-change", %{"value" => value}, socket) do
-    socket =
-      if socket.assigns.datapoint_parse_error do
-        case parse_datapoint(value) do
-          {:ok, _} -> assign(socket, :datapoint_parse_error, nil)
-          {:error, _} -> socket
-        end
-      else
-        socket
-      end
-
-    {:noreply, socket}
-  end
-
   def handle_event(
         "update-datapoint",
         %{"value" => value, "comment" => comment, "id" => id},
         socket
       ) do
-    send(socket.root_pid, {:update_datapoint, id, value, comment})
+    send(self(), {:update_datapoint, id, value, comment})
 
     socket =
       socket
@@ -111,7 +48,7 @@ defmodule AltbeeWeb.GoalLive do
   end
 
   def handle_event("delete-datapoint", %{"datapoint-id" => id}, socket) do
-    send(socket.root_pid, {:delete_datapoint, id})
+    send(self(), {:delete_datapoint, id})
 
     socket =
       socket
@@ -121,42 +58,13 @@ defmodule AltbeeWeb.GoalLive do
   end
 
   def handle_event("refresh", _, socket) do
-    send(socket.root_pid, :refresh)
+    send(self(), :refresh)
 
     socket =
       socket
       |> assign(:waiting, true)
 
     {:noreply, socket}
-  end
-
-  def daystamp_from_diff(diff, timezone, deadline) do
-    date_from_diff(diff, timezone, deadline)
-    |> Timex.format!("{YYYY}{0M}{0D}")
-  end
-
-  def day_text_from_diff(diff, timezone, deadline) do
-    date = date_from_diff(diff, timezone, deadline)
-
-    case diff do
-      0 ->
-        "Today (#{date})"
-
-      1 ->
-        "Yesterday (#{date})"
-
-      _ ->
-        day = Timex.day_name(Date.day_of_week(date))
-        "#{day} (#{date})"
-    end
-  end
-
-  defp date_from_diff(diff, timezone, deadline) do
-    Timex.now()
-    |> Timex.Timezone.convert(timezone)
-    |> Timex.shift(days: -diff)
-    |> Timex.shift(seconds: -deadline)
-    |> Timex.to_date()
   end
 
   defp waiting?(%Phoenix.LiveView.Socket{} = socket) do
@@ -169,23 +77,33 @@ defmodule AltbeeWeb.GoalLive do
 
   defp waiting?(_), do: false
 
-  defp enqueue_goal_poll(socket) do
-    Process.send_after(socket.root_pid, :poll_queued_goal, @goal_poll_interval)
+  defp enqueue_goal_poll() do
+    Process.send_after(self(), :poll_queued_goal, @goal_poll_interval)
   end
 
   defp watch_goal(socket) do
     if connected?(socket) && waiting?(socket) do
-      enqueue_goal_poll(socket)
+      enqueue_goal_poll()
     end
 
     socket
   end
 
+  def handle_info(:new_datapoint, socket) do
+    socket =
+      socket
+      |> assign(:waiting, true)
+
+    enqueue_goal_poll()
+
+    {:noreply, socket}
+  end
+
   def handle_info(:poll_queued_goal, socket) do
-    send(socket.root_pid, :refresh)
+    send(self(), :refresh)
 
     if waiting?(socket) do
-      enqueue_goal_poll(socket)
+      enqueue_goal_poll()
     end
 
     {:noreply, socket}
@@ -229,7 +147,7 @@ defmodule AltbeeWeb.GoalLive do
   def handle_info({:delete_datapoint, id}, socket) do
     %{goal: goal, user: user} = socket.assigns
 
-    enqueue_goal_poll(socket)
+    enqueue_goal_poll()
 
     Task.start(fn ->
       Datapoints.delete_datapoint!(goal["slug"], id, user.access_token)
@@ -241,7 +159,7 @@ defmodule AltbeeWeb.GoalLive do
   def handle_info({:update_datapoint, id, value, comment}, socket) do
     %{goal: goal, user: user} = socket.assigns
 
-    enqueue_goal_poll(socket)
+    enqueue_goal_poll()
 
     Task.start(fn ->
       Datapoints.update_datapoint!(goal["slug"], id, user.access_token, value, comment)
