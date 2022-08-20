176911cea621455722138df0f5c79be409526f40
Benjamin Fox <benjaminfox@hey.com>
2022-08-20 13:28:09 +0300

    Switch from nprogress to topbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,9 @@
-import '../css/app.css'
-
 import 'phoenix_html'
 
 import './alpine'
 import './filter'
 import './forms'
 import './live-view'
-import './progress'
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js')
--- assets/js/live-view/index.js
+++ assets/js/live-view/index.js
@@ -2,7 +2,7 @@ import { Socket } from 'phoenix'
 import { LiveSocket } from 'phoenix_live_view'
 import csrfToken from '../csrf-token'
 import Alpine from '../alpine'
-
+import topbar from 'topbar'
 import hooks from './hooks'
 
 const liveSocket = new LiveSocket('/live', Socket, {
@@ -13,6 +13,20 @@ const liveSocket = new LiveSocket('/live', Socket, {
     }
   },
   params: { _csrf_token: csrfToken }
+})
+
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
+
+let topbarTimeout = null
+
+window.addEventListener("phx:page-loading-start", info => {
+  clearTimeout(topbarTimeout)
+  topbarTimeout = setTimeout(() => topbar.show(), 800)
+})
+
+window.addEventListener("phx:page-loading-stop", info => {
+  clearTimeout(topbarTimeout)
+  topbar.hide()
 })
 
 liveSocket.connect()
